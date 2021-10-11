c7d7f738a6bc3d2f6a39ba5af3551d06f8fd557d
JAE-GYU <shin3372@icloud.com>
2021-10-11 17:46:59 +0900

    setup eslint, prettier, stylelint, husky, lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  // https://eslint.org/docs/rules/no-undef#nodejs
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  // https://github.com/vuejs/vue-eslint-parser#parseroptionsparser
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+}
